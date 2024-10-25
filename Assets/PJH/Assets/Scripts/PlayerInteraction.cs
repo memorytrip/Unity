481e434cff711a5718e1cb59009479cbd4c2006d
Junghoon357 <never1362@gmail.com>
2024-10-25 11:05:48 +0900

    사진 obj 동기화 진짜끝

--- Assets/PJH/Assets/Scripts/PlayerInteraction.cs
+++ Assets/PJH/Assets/Scripts/PlayerInteraction.cs
@@ -18,16 +18,20 @@ public class PlayerInteraction : NetworkBehaviour
     void Update()
     {
         CheckForInteractable();
+        PostNotice();
     }
 
-    public void CheckForInteractable()
+    private bool CheckForInteractable()
     {
         // Physics.SphereCast는 bool 값을 반환하므로, hit에 bool이 들어가는 문제를 수정.
-        bool isHit = Physics.SphereCast(transform.position, interactionRadius, transform.forward, out hits, interactionRange, interactionLayer);
+        return Physics.SphereCast(transform.position, interactionRadius, transform.forward, out hits, interactionRange, interactionLayer);
+    }
 
+    private void PostNotice()
+    {
         if (HasStateAuthority)
         {
-            if (isHit)
+            if (CheckForInteractable())
             {
                 {
                     EventManager.Instance.PostNotification(EventType.eRaycasting, this, new RaycastInfo(true, hits));
@@ -39,7 +43,6 @@ public class PlayerInteraction : NetworkBehaviour
             }
         }
     }
-    
     public struct RaycastInfo
     {
         public bool isHit;
@@ -51,21 +54,5 @@ public class PlayerInteraction : NetworkBehaviour
             this.hitInfo = hitInfo;
         }
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-
-        // 시작 위치에서 반경만큼의 구를 그려줍니다.
-        Gizmos.DrawWireSphere(transform.position, interactionRadius);
-
-        // SphereCast가 진행될 방향과 끝 위치를 계산합니다.
-        Vector3 direction = transform.forward;
-        Vector3 endPosition = transform.position + direction * interactionRange;
-
-        // 구체 레이 시작 지점에서 끝 지점까지 레이를 그립니다.
-        Gizmos.DrawLine(transform.position, endPosition);
-
-        // 끝 위치에서 구체를 그려주어 SphereCast의 범위를 나타냅니다.
-        Gizmos.DrawWireSphere(endPosition, interactionRadius);    }
+    
 }
