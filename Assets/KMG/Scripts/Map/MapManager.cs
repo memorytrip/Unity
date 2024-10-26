d863ca0111977d0d74126e22b9ee2f3b4ab0a51a
tgds03 <xyzuct6800@naver.com>
2024-10-26 15:27:24 +0900

    241026

--- Assets/KMG/Scripts/Map/MapManager.cs
+++ Assets/KMG/Scripts/Map/MapManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using Common;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
@@ -19,12 +20,17 @@ namespace Map
         }
         private MapManager()
         {
-            if (_instance != null) return;
+            if (_instance != null) 
+                return;
         }
 
+#region Load MapList, MapInfo
         public async UniTask<List<MapInfo>> LoadMapList(User user)
         {
-            return await LoadDefaultsMapList();
+            List<MapInfo> mapInfos = new List<MapInfo>(4);
+            mapInfos.AddRange(await LoadDefaultsMapList());
+            mapInfos.AddRange(await LoadCustomMapList());
+            return mapInfos;
         }
         
         private async UniTask<List<MapInfo>> LoadDefaultsMapList()
@@ -36,15 +42,38 @@ namespace Map
             return mapInfos;
         }
 
+        private async UniTask<List<MapInfo>> LoadCustomMapList()
+        {
+            List<MapInfo> mapInfos = new List<MapInfo>();
+            
+            DirectoryInfo directoryInfo = new DirectoryInfo($"{Application.persistentDataPath}/Maps/");
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            foreach (var mapJson in directoryInfo.GetFiles("*.json"))
+            {
+                StreamReader reader = mapJson.OpenText();
+                MapInfo mapInfo = JsonConvert.DeserializeObject<MapInfo>(await reader.ReadToEndAsync());
+                mapInfos.Add(mapInfo);
+            }
+
+            return mapInfos;
+        }
+
         private async UniTask<string> LoadMapFileFromResources(string mapName)
         {
             TextAsset textAsset = await Resources.LoadAsync<TextAsset>("Maps/" + mapName) as TextAsset;
             return textAsset.text;
         }
+#endregion
         
+#region Load MapConcrete        
         public async UniTask<MapConcrete> LoadMap(MapInfo mapInfo)
         {
             return await MapConverter.ConvertMapInfoToMapConcrete(mapInfo);
         }
+#endregion
+        
     }
 }
