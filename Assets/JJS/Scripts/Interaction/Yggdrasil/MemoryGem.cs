063b3f206364837273d4ba905e356885856bb4c7
Jinsol <jinsoljungjs@gmail.com>
2024-11-05 17:16:50 +0900

    스크린샷 카메라의 패닝 설정 (그런데 이제 추가 이슈를 곁들인) 스크린샷 활성화 -> 패닝 -> 모든 UI 버튼이 안눌림

--- Assets/JJS/Scripts/Interaction/Yggdrasil/MemoryGem.cs
+++ Assets/JJS/Scripts/Interaction/Yggdrasil/MemoryGem.cs
@@ -4,25 +4,6 @@ using UnityEngine.UI;
 //[RequireComponent(typeof(Button))]
 public class MemoryGem : MonoBehaviour, IClickable3dObject
 {
-    private Button _button;
-
-    private void Awake()
-    {
-        //_button = GetComponent<Button>();
-    }
-
-    private void OnEnable()
-    {
-        /*var yggdrasil = FindAnyObjectByType<ToggleYggdrasil>();
-        _button.onClick.AddListener(yggdrasil.DisplayContentUi);
-        _button.onClick.AddListener(OnClick);*/
-    }
-
-    private void OnDisable()
-    {
-        //_button.onClick.RemoveAllListeners();
-    }
-
     public void OnClick()
     {
         Debug.Log("OnClick");
--- Assets/JJS/Scripts/UI/Clickable3dObjects.cs
+++ Assets/JJS/Scripts/UI/Clickable3dObjects.cs
@@ -1,32 +1,31 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class Clickable3dObjects : MonoBehaviour
 {
-    // LayerMask to limit raycast to specific layers, if desired
     [SerializeField] private LayerMask clickableLayerMask;
 
-    void Update()
+    private void OnEnable()
     {
-        // Check for mouse click
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            // Create a ray from the camera to the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Touch.onFingerDown += CheckMouseClick;
+    }
 
-            // Check if the ray hits any object in the scene
-            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, clickableLayerMask))
+    private void CheckMouseClick(Finger finger)
+    {
+        foreach (var touch in Touch.activeTouches)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touch.screenPosition);
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, clickableLayerMask))
             {
-                // If the hit object has a specific component, interact with it
-                var clickable = hit.collider.GetComponent<IClickable3dObject>();
-                if (clickable != null)
-                {
-                    clickable.OnClick();
-                }
-                else
-                {
-                    Debug.Log("3D Object clicked: " + hit.collider.name);
-                }
+                return;
+            }
+            
+            var clickable = hit.collider.GetComponent<IClickable3dObject>();
+            if (clickable != null)
+            {
+                clickable.OnClick();
             }
         }
     }
