c4a49d640ebf11e18ee1e014f999a612f3e2de9e
Jinsol <jinsoljungjs@gmail.com>
2024-11-08 14:10:45 +0900

    로그인 연출 수정

--- Assets/Scripts/DummyLoginUI.cs
+++ Assets/Scripts/DummyLoginUI.cs
@@ -9,13 +9,19 @@ public class DummyLoginUI : MonoBehaviour
     [SerializeField] private TMP_InputField idInputField;
     [SerializeField] private TMP_InputField passwordInputField;
     private readonly WaitForSeconds _wait = new WaitForSeconds(1f);
+    private readonly WaitForSeconds _typeDelay = new WaitForSeconds(0.05f);
     [SerializeField] private Button triggerButton;
     [SerializeField] private Button triggerClearButton;
+    private const string IdKey = "Traver";
+    private const string PasswordKey = "Pass!1234"; // 기절할 비밀번호 조합
 
     private void Awake()
     {
         triggerButton.onClick.AddListener(StartInput);
         triggerClearButton.onClick.AddListener(ClearInput);
+        idInputField.caretWidth = passwordInputField.caretWidth = 2;
+        idInputField.caretBlinkRate = passwordInputField.caretBlinkRate = 2f;
+        idInputField.caretColor = passwordInputField.caretColor = idInputField.colors.normalColor;
     }
     
     public void StartInput()
@@ -26,14 +32,28 @@ public class DummyLoginUI : MonoBehaviour
     private IEnumerator AutomateInput()
     {
         yield return _wait;
-        idInputField.text = "Traver";
+        idInputField.ActivateInputField();
+        foreach (var character in IdKey.ToCharArray())
+        {
+            idInputField.text += character;
+            idInputField.caretPosition = idInputField.text.Length;
+            yield return _typeDelay;
+        }
         yield return _wait;
-        passwordInputField.text = "Password!1234"; // 기절할 비밀번호 조합
+        passwordInputField.ActivateInputField();
+        foreach (var character in PasswordKey.ToCharArray())
+        {
+            passwordInputField.text += character;
+            passwordInputField.caretPosition = passwordInputField.text.Length;
+            yield return _typeDelay;
+        }
     }
 
     public void ClearInput()
     {
         idInputField.text = string.Empty;
         passwordInputField.text = string.Empty;
+        idInputField.DeactivateInputField();
+        passwordInputField.DeactivateInputField();
     }
 }
