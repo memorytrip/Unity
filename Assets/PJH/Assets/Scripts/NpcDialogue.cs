c9fad8d8dfe345ecf3b50035fcc7fedf3700e471
tgds03 <xyzuct6800@naver.com>
2024-11-27 12:07:36 +0900

    241127 버그 수정

--- Assets/PJH/Assets/Scripts/NpcDialogue.cs
+++ Assets/PJH/Assets/Scripts/NpcDialogue.cs
@@ -22,18 +22,18 @@ public class TouchInput : MonoBehaviour
 
         else
         {
-            if (Input.touchCount > 0)
+            var activeTouches = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches;
+            if (activeTouches.Count > 0)
             {
-                Touch touch = Input.GetTouch(0);
-            
-                if (touch.phase == TouchPhase.Began)
+                var touch = activeTouches[0];
+                if (touch.began)
                 {
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    Ray ray = Camera.main.ScreenPointToRay(touch.screenPosition);
+                    if (Physics.Raycast(ray, out RaycastHit hit, LayerMask.GetMask("NPC")));
                     {
                         // 터치된 오브젝트 확인
                         Debug.Log($"Touched {hit.collider.gameObject.name}");
-
+                    
                         // 특정 오브젝트에 반응하도록 설정
                         if (hit.collider.CompareTag("Touchable"))
                         {
@@ -43,6 +43,28 @@ public class TouchInput : MonoBehaviour
                     }
                 }
             }
+
+            // if (Input.touchCount > 0)
+            // {
+            //     Touch touch = Input.GetTouch(0);
+            //
+            //     if (touch.phase == TouchPhase.Began)
+            //     {
+            //         Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            //         if (Physics.Raycast(ray, out RaycastHit hit))
+            //         {
+            //             // 터치된 오브젝트 확인
+            //             Debug.Log($"Touched {hit.collider.gameObject.name}");
+            //
+            //             // 특정 오브젝트에 반응하도록 설정
+            //             if (hit.collider.CompareTag("Touchable"))
+            //             {
+            //                 UIManager.ShowUI(dialogue);
+            //                 exclamation.SetActive(false);
+            //             }
+            //         }
+            //     }
+            // }
         }
     }
 
