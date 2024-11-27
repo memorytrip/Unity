929fa7b754b547f1019cbfe0b8576c1704bea610
tgds03 <xyzuct6800@naver.com>
2024-11-27 20:34:09 +0900

    241127 생?성된 영상 소리 재생

--- Assets/PJH/Assets/Scripts/Video/VideoPlay.cs
+++ Assets/PJH/Assets/Scripts/Video/VideoPlay.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -7,10 +8,27 @@ public class VideoPlay : MonoBehaviour
     public CanvasGroup playButton;
     public CanvasGroup resumeButton;
     [SerializeField] private bool isPlaying = false;
+    
+    private AudioSource audioSource;
 
     private void Awake()
     {
         isPlaying = false;
+    }
+
+    private void Start()
+    {
+        InitializeVideoPlayer();
+    }
+    
+    private void InitializeVideoPlayer()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Debug.Log(audioSource);
+        player.playOnAwake = false;
+        player.isLooping = true;
+        player.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        player.SetTargetAudioSource(0, audioSource);
     }
 
     public void VideoPlayControll()
@@ -27,5 +45,23 @@ public class VideoPlay : MonoBehaviour
             player.Pause();
             isPlaying = false;
         }
+    }
+
+    public void Play()
+    {
+        if (player.isPlaying) 
+            return;
+        CustomFmodAmbienceManager.Instance.StopPlayback();
+        CustomFmodBgmManager.Instance.StopPlayback();
+        player.Play();
+    }
+    
+    public void Pause()
+    {
+        if (!player.isPlaying) 
+            return;
+        CustomFmodAmbienceManager.Instance.ResumePlayback();
+        CustomFmodBgmManager.Instance.ResumePlayback();
+        player.Pause();
     }
 }
