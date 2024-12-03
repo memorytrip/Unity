bde9695699d97ae2b245b3981c9d7ea5501129b7
tgds03 <xyzuct6800@naver.com>
2024-12-03 17:05:08 +0900

    241203 텔레포트 추가

---
+++ Assets/KMG/Scripts/TeleportButton.cs
@@ -0,0 +1,20 @@
+using Common.Network;
+using Fusion;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeleportButton : MonoBehaviour
+{
+	[SerializeField] private Transform destination;
+    private Button button;
+
+	private void Start() {
+		button = GetComponent<Button>();
+		button.onClick.AddListener(Teleport);
+	}
+
+	private void Teleport() {
+		NetworkTransform networkTransform = Connection.StateAuthInstance.currenctCharacter.GetComponent<NetworkTransform>();
+		networkTransform.Teleport(destination.position);
+	}
+}
