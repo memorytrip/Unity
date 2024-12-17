192193f3f736deb9b20f232c1234a71fcfb5fc7a
Junghoon357 <117274195+Junghoon357@users.noreply.github.com>
2024-12-17 16:14:37 +0900

    웹소켓 주소 변경

--- Assets/PJH/Assets/Scripts/PhotoSelect/WebSocketManager.cs
+++ Assets/PJH/Assets/Scripts/PhotoSelect/WebSocketManager.cs
@@ -16,7 +16,7 @@ public class WebSocketManager : MonoBehaviour
     private CancellationTokenSource _cancellationTokenSource;
 
     private WebSocket _webSocket;
-    private string _url = "ws://memorytrip-env.eba-73mrisxy.ap-northeast-2.elasticbeanstalk.com/ws?token=";// 서버 URL
+    private string _url = "125.132.216.190:17778/ws?token=";// 서버 URL
     public bool IsConnected => _webSocket != null && _webSocket.ReadyState == WebSocketState.Open;
 
     private void Awake()
