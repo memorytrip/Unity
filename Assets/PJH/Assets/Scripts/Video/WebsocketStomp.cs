df221060d1885d727af7141ade64a27364a9b9e2
tgds03 <xyzuct6800@naver.com>
2024-12-18 10:28:25 +0900

    241218 stomp subscribe rpc로 변경

--- Assets/PJH/Assets/Scripts/Video/WebsocketStomp.cs
+++ Assets/PJH/Assets/Scripts/Video/WebsocketStomp.cs
@@ -1,12 +1,19 @@
 using Common.Network;
+using Fusion;
 using UnityEngine;
 
-public class WebsocketStomp : MonoBehaviour
+public class WebsocketStomp : NetworkBehaviour
 {
     public void GetStompData()
     { 
+        RpcStompSubscribe();
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    public void RpcStompSubscribe()
+    {
+        Debug.Log("Stomp Subscribe");
         WebSocketManager.Instance._webSocket.Send("CONNECT\naccept-version:1,2\nhost:125.132.216.190:17778\n\n\0");
         WebSocketManager.Instance._webSocket.Send($"SUBSCRIBE\nid:sub-0\ndestination:/topic/videos/{RunnerManager.Instance.Runner.SessionInfo.Name}\n\n\0");
-        
     }
 }
